89698501876033e4c762afb7c08452bcabe0e3b5
Ant Man <34754995+aaronamendez@users.noreply.github.com>
2022-02-11 13:15:12 -0500

    Finished project setup, finished two routers for the post.

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,52 @@
 // Write your "projects" router here!
+const express = require('express');
+const projectsRouter = express.Router();
+const Projects = require('./projects-model');
+
+projectsRouter.get('/', async (req, res) => {
+	try {
+		Projects.get().then((projects) => {
+			res.status(200).json(projects);
+		});
+	} catch (err) {
+		res.status(500).json('Internal Server Error');
+	}
+});
+
+projectsRouter.get('/:id', async (req, res) => {
+	try {
+		const { id } = req.params;
+
+		Projects.get(id).then((project) => {
+			if (project !== null) {
+				res.status(200).json(project);
+			} else {
+				res.status(404).json('Project Not Found');
+			}
+		});
+	} catch (err) {
+		res.status(500).json('Internal Server Error');
+	}
+});
+
+projectsRouter.post('/', async (req, res) => {
+	try {
+		const { name, description } = req.body;
+
+		if (name && description) {
+			const obj = {
+				name,
+				description,
+			};
+			Projects.insert(obj).then((project) => {
+				res.status(200).json(project);
+			});
+		} else {
+			res.status(400).json({ message: 'Name and Description are required!' });
+		}
+	} catch (err) {
+		res.status(500).json('Internal Server Error');
+	}
+});
+
+module.exports = projectsRouter;
--- api/server.js
+++ api/server.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const server = express();
+const projectsRouter = require('./projects/projects-router');
+
+server.use(express.json());
+server.use('/api/projects', projectsRouter);
+
+server.get('/', async (req, res) => {
+	try {
+		res.json('OK');
+	} catch (err) {
+		res.status(500).json('Internal Server Error');
+	}
+});
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
