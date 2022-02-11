e8c9d55b96fb448cf2ef4f44d413f205bc044241
Ant Man <34754995+aaronamendez@users.noreply.github.com>
2022-02-11 15:38:51 -0500

    Finished the Actions Router, moving on to interview questions.

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,26 @@
 // add middlewares here related to actions
+const Actions = require('./actions-model');
+
+const validateId = async (req, res, next) => {
+	const { id } = req.params;
+	Actions.get(id).then((action) => {
+		if (action) {
+			req.action = action;
+			next();
+		} else {
+			res.status(404).json({ message: 'No action found!' });
+		}
+	});
+};
+
+const validateBody = async (req, res, next) => {
+	const { description, notes, completed, project_id } = req.body;
+	if (description && notes && project_id && completed !== undefined) {
+		req.actionBody = req.body;
+		next();
+	} else {
+		res.status(400).json({ message: 'Invalid Body!' });
+	}
+};
+
+module.exports = { validateId, validateBody };
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -3,11 +3,57 @@
 const express = require('express');
 const actionsRouter = express.Router();
 const Actions = require('./actions-model');
+const { validateId, validateBody } = require('./actions-middlware');
 
 actionsRouter.get('/', async (req, res) => {
 	try {
-		Actions.get().then((projects) => {
-			res.status(200).json(projects);
+		Actions.get().then((actions) => {
+			res.status(200).json(actions);
+		});
+	} catch (err) {
+		res.status(500).json('Internal Server Error');
+	}
+});
+
+actionsRouter.get('/:id', validateId, async (req, res) => {
+	try {
+		res.status(200).json(req.action);
+	} catch (err) {
+		res.status(500).json('Internal Server Error');
+	}
+});
+
+actionsRouter.post('/', validateBody, async (req, res) => {
+	try {
+		Actions.get(req.actionBody.project_id).then((exists) => {
+			if (exists) {
+				// Continue ...
+				Actions.insert(req.actionBody).then((newAction) => {
+					res.status(201).json(newAction);
+				});
+			} else {
+				res.status(404).json({ message: "That project doesn't exist!" });
+			}
+		});
+	} catch (err) {
+		res.status(500).json('Internal Server Error');
+	}
+});
+
+actionsRouter.put('/:id', validateId, validateBody, async (req, res) => {
+	try {
+		Actions.update(req.action.id, req.actionBody).then((updated) => {
+			res.status(200).json(updated);
+		});
+	} catch (err) {
+		res.status(500).json('Internal Server Error');
+	}
+});
+
+actionsRouter.delete('/:id', validateId, async (req, res) => {
+	try {
+		Actions.remove(req.action.id).then((deleted) => {
+			res.status(200).json(deleted);
 		});
 	} catch (err) {
 		res.status(500).json('Internal Server Error');
